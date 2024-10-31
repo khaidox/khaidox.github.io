68142e1c3f0d4ceb92d9fb659b8c1e8b1caaa514
Khai Do <dovankhai97@gmail.com>
2024-10-31 21:09:32 +0700

    first post

---
+++ test.sh
@@ -0,0 +1,8 @@
+c7f9675 Cotes Chung ci: skip test for invalid PRs (#2013) (5 days ago)
+74ed063 Cotes Chung ci: block invalid pull requests (#2010) (6 days ago)
+d4f7f39 Cotes Chung refactor: simplify sidebar animation (10 days ago)
+c1bd9eb Cotes Chung refactor: reduce duplicate scss (11 days ago)
+
+
+JEKYLL_ENV=production bundle exec jekyll build
+bundle exec jekyll serve
